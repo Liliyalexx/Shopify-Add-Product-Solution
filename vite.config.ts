326e94c3dbdd2b22383ac0a0cbe56d8867a65597
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-04-21 10:23:55 -0700

    deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: '/shopify_addproduct_inhancer/',
+  base: '/Shopify-Add-Product-Solution/',
   server: {
     host: "::",
     port: 8080,
