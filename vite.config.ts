a946ff68da12db7bee49b44b4da9a145f1b1eb32
liliyafedyurina <liliya.fedyurina@gmail.com>
2025-04-21 19:33:43 -0700

    Setup deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(() => ({
-  base: '/',
+  base: '/Shopify-Add-Product-Solution/',  // ← this should match your repo name!
   server: {
     host: "::",
     port: 8080,
